a6908762e17483545ea565433dd9d7904e56ac78
Viktor Tasevski <viktor-tasevski@hotmail.com>
2023-09-06 10:42:06 +0200

    Fix ingress.yml

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,8 +1,8 @@
 # without ingress
-k3d cluster create room-revamp -p "3000:3000@loadbalancer" -p "8000:8000@loadbalancer" -p "5432:5432@loadbalancer" -s 1 -a 1
+# k3d cluster create room-revamp -p "3000:3000@loadbalancer" -p "8000:8000@loadbalancer" -p "5432:5432@loadbalancer" -s 1 -a 1
 
 # with ingress
-# k3d cluster create room-revamp -p "80:80@loadbalancer" -s 1 -a 1
+k3d cluster create room-revamp -p "80:80@loadbalancer" -s 1 -a 1
 
 kubectl create namespace room-revamp
 
